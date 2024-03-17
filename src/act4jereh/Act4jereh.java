2a744170514c4c4f783875adc630dccd57606059
User <User@DESKTOP-PS1R47A>
2024-03-17 11:05:12 +0800

    Activity 4

---
+++ src/act4jereh/Act4jereh.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package act4jereh;
+
+/**
+ *
+ * @author User
+ */
+public class Act4jereh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int entranceScore = 75;
+        double gpa = 3.5;
+        
+        if (entranceScore == gpa) {
+            System.out.println("Eligible for admission.");
+        }
+        else {
+            System.out.println("Not eligible for admission.");
+
+   
+        }
+    }   
+    
+}
